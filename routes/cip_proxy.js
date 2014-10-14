e016383a49a668031618a651b377fb6f1d5c83c1
Kræn Hansen (BIT BLUEPRINT) <kh@bitblueprint.com>
2014-10-14 15:13:50 +0200

    Changing the www-authenticate header returned from a call to the CIP.

--- routes/cip_proxy.js
+++ routes/cip_proxy.js
@@ -4,6 +4,17 @@ var cip = require('../lib/cip-natmus');
 
 router.all('/*', function(req, res) {
   var url = cip.config.endpoint + req.url.substring(1);
+
+  // Replace the 'www-authenticate' header to prevent browsers to
+  // popup an authentication dialog.
+  res.originalWriteHead = res.writeHead;
+  res.writeHead = function(statusCode, reasonPhrase, headers) {
+  	if(statusCode === 401) {
+	    res.header('www-authenticate', 'CIPBasic realm="CIP"');
+	  }
+    res.originalWriteHead(statusCode, reasonPhrase, headers);
+  }
+
   req.pipe(request(url)).pipe(res);
 });
 
