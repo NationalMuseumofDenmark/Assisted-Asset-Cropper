4d19e5ccd93ad6ee90432eb38c9f673b926ca66f
Kræn Hansen (BIT BLUEPRINT) <kh@bitblueprint.com>
2014-10-21 08:18:50 +0200

    Adding the overview search from location.hash.

--- public/javascripts/partials/searchbar.js
+++ public/javascripts/partials/searchbar.js
@@ -5,6 +5,16 @@
 			console.error("The searchbar partial expects a global function called perform_search.");
 		}
 
+		function select_catalog_from_alias(catalog_alias) {
+			$catalogSelector = $("ul[aria-labelledby='catalog-selector']");
+			$catalogSelector.find("a[role=menuitem]").each(function() {
+				var $catalog = $(this);
+				if($catalog.data('alias') == catalog_alias) {
+					select_catalog($catalog);
+				}
+			});
+		}
+
 		function select_catalog($catalog) {
 			$catalogSelector = $("#catalog-selector");
 			$catalogSelector
@@ -13,7 +23,28 @@
 				.find(".text .catalog").text($catalog.text());
 		}
 
-		function update_catalogs_dropdown(catalogs) {
+		function set_search_location_hash(search_term, catalog_alias) {
+			location.hash = catalog_alias + "/" + search_term;
+		}
+
+		function check_search_location_hash() {
+			var hash = location.hash.substring(1);
+			var lastSlashIndex = hash.indexOf("/");
+			if(lastSlashIndex >= 0) {
+				var catalog_alias = hash.substring(0, lastSlashIndex);
+				var search_term = hash.substring(lastSlashIndex + 1);
+				// console.log("search_term =", search_term, "catalog_alias =", catalog_alias);
+				if(catalog_alias !== "") {
+					$("#search-term").val(search_term);
+					select_catalog_from_alias(catalog_alias);
+					pre_perform_search();
+					return true;
+				}
+			}
+			return false;
+		}
+
+		function update_catalogs_dropdown(catalogs, select_first) {
 			$catalogSelector = $("ul[aria-labelledby='catalog-selector']");
 			$catalogSelector.empty();
 
@@ -26,20 +57,24 @@
 					.text(catalog.name)
 					.click(function() {
 						select_catalog( $(this) );
+						pre_perform_search();
 					});
 				$catalogItem = $("<li>")
 					.attr("role", "presentation")
 					.append($catalogAnchor);
 				$catalogSelector.append($catalogItem);
 			}
-			var $firstCatalog = $catalogSelector.find("a[role=menuitem]:first");
-			select_catalog( $firstCatalog );
+			if(select_first === true || typeof(select_first) === "undefined") {
+				var $firstCatalog = $catalogSelector.find("a[role=menuitem]:first");
+				select_catalog( $firstCatalog );
+			}
 		}
 
 		function pre_perform_search() {
 			var search_term = $("#search-term").val();
 			$catalogSelector = $("#catalog-selector");
 			var catalog_alias = $catalogSelector.data('selected-alias');
+			set_search_location_hash(search_term, catalog_alias);
 			perform_search(search_term, catalog_alias);
 		}
 
@@ -53,9 +88,14 @@
 			*/
 			// Update the catalogs dropdown menu items.
 			update_catalogs_dropdown(catalogs);
+			// Check the location hash, to see if the URL refers to
+			// a concrete search.
+			var had_location_hash = check_search_location_hash();
 			// Perform a simple search, in the initially selected catalog and
 			// the empty search term.
-			pre_perform_search();
+			if(had_location_hash !== true) {
+				pre_perform_search();
+			}
 			$("#search-term").keyup(function(e) {
 				if(e.keyCode == 13) {
 					pre_perform_search();
