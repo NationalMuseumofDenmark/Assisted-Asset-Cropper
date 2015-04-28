a0c1baa4abd0f9e7348094d03b29f8587a24f625
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2015-04-28 10:01:22 +0200

    Removed unused imports and refactored out the image selection area.

--- frontend/public/javascripts/controllers/assetCtrl.js
+++ frontend/public/javascripts/controllers/assetCtrl.js
@@ -7,97 +7,8 @@
 			$scope.asset_id = $stateParams.asset_id;
 
 			$scope.asset = asset;
-			$scope.thumbnail = {
-				loaded: false,
-				width: 1000, // Fake it, till we load it.
-				height: 1000,
-				url: asset.get_thumbnail_url()
-			};
 
 			$scope.croppings = [];
-			$scope.cropping_selected = false;
-
-			$scope.handle_padding = 5;
-			$scope.rotational_handle_radius = 10;
-			$scope.rotational_handle_length = 30;
-
-			function cropping(location) {
-				this.center_x = location.center_x;
-				this.center_y = location.center_y;
-				this.width = location.width;
-				this.height = location.height;
-				this.rotation = location.rotation;
-
-				var valid_center_x = this.center_x >= 0 && this.center_x <= 1;
-				var valid_center_y = this.center_y >= 0 && this.center_y <= 1;
-				var valid_width = this.width >= 0 && this.width <= 1;
-				var valid_height = this.height >= 0 && this.height <= 1;
-				if(valid_center_x && valid_center_y && valid_width && valid_height) {
-					$scope.croppings.push(this);
-				} else {
-					throw new Error("Trying to create an invalid cropping.");
-				}
-			}
-
-			function deselectCroppings() {
-				$scope.croppings.forEach(function(cropping) {
-					cropping.deselect();
-				});
-			}
-
-			function removeSelectedCropping() {
-				$scope.croppings.forEach(function(cropping) {
-					if(cropping.selected) {
-						cropping.remove();
-					}
-				});
-			}
-
-			cropping.prototype.select = function() {
-				deselectCroppings();
-				$scope.cropping_selected = true;
-				this.selected = true;
-			};
-
-			cropping.prototype.deselect = function() {
-				$scope.cropping_selected = false;
-				this.selected = false;
-			};
-
-			cropping.prototype.remove = function() {
-				this.deselect();
-				var to_be_removed = this;
-				$scope.croppings = $scope.croppings.filter(function(cropping) {
-					return cropping !== to_be_removed;
-				});
-			};
-
-			cropping.prototype.grabOutlineHandle = function(direction) {
-				var to_be_grabbed = this;
-				$scope.croppings.forEach(function(cropping) {
-					if(cropping === to_be_grabbed) {
-						// Let's wait for outline to create itself, if this was
-						// called, just after creating the cropping.
-						setTimeout(function() {
-							cropping.outline.handleGrabbed(direction);
-						}, 1);
-					}
-				});
-			};
-
-			$scope.dragCreateCropping = function(x, y) {
-				$scope.dragging_canvas = false;
-				var location = {
-					center_x: x,
-					center_y: y,
-					width: 0,
-					height: 0,
-					rotation: 0
-				};
-				var c = new cropping(location);
-				c.select();
-				c.grabOutlineHandle('south-east');
-			};
 
 			// Get all the cropping suggestions.
 			var load_suggested_croppings_url = [
@@ -112,8 +23,9 @@
 			$scope.loadSuggestedCroppings = function() {
 				// Fetch all suggested croppings
 				$http.get(load_suggested_croppings_url).then(function(response) {
-					response.data.forEach( function(location) {
-						new cropping(location);
+					response.data.forEach( function(selection) {
+						//selection.rotation = 10;
+						$scope.croppings.push(selection);
 					} );
 				});
 			};
@@ -166,124 +78,8 @@
 					$scope.showMessage( 'danger', message );
 				});
 			};
-
-			$(window).keypress(function(e) {
-				if(e.keyCode === 127) { // The 'Delete' button
-					removeSelectedCropping();
-				}
-			});
-
-			// When clicking anywhere inside the canvas, don't propagate.
-			$("#canvas, #suggestions, #outlines").click(function(e) {
-				e.stopPropagation();
-			});
-			// When clicking anywhere outside the canvas, deselect all croppings.
-			$(document.body).click(function() {
-				$scope.$apply(function() {
-					deselectCroppings();
-				});
-			});
-	}])
-	.directive('croppingOutline', function() {
-		return {
-			restrict: 'E',
-			templateUrl: 'templates/partials/cropping-outline.html',
-			replace: true,
-			templateNamespace: 'svg',
-			controller: ['$scope', function($scope) {
-
-				this.direction = undefined;
-				this.grabbed = false;
-				this.grabbed_at = {
-					x: undefined,
-					y: undefined
-				};
-				this.grabbed_offset = {
-					top: undefined,
-					left: undefined,
-					bottom: undefined,
-					right: undefined
-				};
-
-				this.handleGrabbed = function(direction, x, y) {
-					if($scope.cropping.selected) {
-						this.grabbed_at = { x: x, y: y };
-						this.grabbed_offset = {
-							center_x: $scope.cropping.center_x,
-							center_y: $scope.cropping.center_y,
-							width: $scope.cropping.width,
-							height: $scope.cropping.height,
-							rotation: $scope.cropping.rotation
-						};
-						this.grabbed = true;
-						this.direction = direction;
-					}
-				};
-
-				this.handleReleased = function() {
-					this.grabbed = false;
-					this.direction = undefined;
-				};
-
-				this.handleMoved = function(x, y) {
-					if($scope.cropping.selected) {
-						if(this.grabbed) {
-							if(this.direction.indexOf('move') >= 0) { // Moving the whole cropping.
-								$scope.cropping.center_x = this.grabbed_offset.center_x - this.grabbed_at.x + x;
-								$scope.cropping.center_y = this.grabbed_offset.center_y - this.grabbed_at.y + y;
-								// Keeing the bounds.
-								$scope.cropping.center_x = Math.max($scope.cropping.width/2, $scope.cropping.center_x);
-								$scope.cropping.center_y = Math.max($scope.cropping.height/2, $scope.cropping.center_y);
-								$scope.cropping.center_x = Math.min(1-$scope.cropping.width/2, $scope.cropping.center_x);
-								$scope.cropping.center_y = Math.min(1-$scope.cropping.height/2, $scope.cropping.center_y);
-							} else {
-								var offsets = {
-									left: $scope.cropping.center_x-$scope.cropping.width/2,
-									top: $scope.cropping.center_y-$scope.cropping.height/2,
-									right: $scope.cropping.center_x+$scope.cropping.width/2,
-									bottom: $scope.cropping.center_y+$scope.cropping.height/2,
-								};
-
-								// TODO: Switch around north and south and east and west.
-								/*
-								if(offsets.left < x) {
-									this.direction = this.direction.replace('west', 'east');
-								} else if(offsets.right > x) {
-									this.direction = this.direction.replace('east', 'west');
-								}
-								if(offsets.top < y) {
-									this.direction = this.direction.replace('south', 'north');
-								} else if(offsets.bottom > y) {
-									this.direction = this.direction.replace('north', 'south');
-								}
-								*/
-
-								if(this.direction.indexOf('north') >= 0) {
-									offsets.top = y;
-								}
-								if(this.direction.indexOf('east') >= 0) {
-									offsets.right = x;
-								}
-								if(this.direction.indexOf('south') >= 0) {
-									offsets.bottom = y;
-								}
-								if(this.direction.indexOf('west') >= 0) {
-									offsets.left = x;
-								}
-								$scope.cropping.center_x = offsets.left+(offsets.right-offsets.left)/2;
-								$scope.cropping.center_y = offsets.top+(offsets.bottom-offsets.top)/2;
-								$scope.cropping.height = Math.max(offsets.bottom - offsets.top, 0 );
-								$scope.cropping.width = Math.max(offsets.right - offsets.left, 0 );
-							}
-						}
-					}
-				};
-
-				$scope.cropping.outline = this;
-			}],
-			controllerAs: 'outline'
-		};
-	})
+	}]);
+	/*
 	.directive('croppingThumbnail', function() {
 		var THUMBNAIL_SIZE = 100;
 
@@ -378,4 +174,5 @@
 			}
 		};
 	});
+	*/
 })();
---
+++ frontend/public/javascripts/controllers/imageSelectionCtrl.js
@@ -0,0 +1,334 @@
+(function() {
+	angular
+	.module('cropper')
+	.controller('imageSelectionCanvasCtrl', ['$scope', function($scope) {
+
+		$scope.image = {
+			loaded: false,
+			src: $scope.imageSrc,
+			width: 1000,
+			height: 1000 // Fake it till you know it.
+		};
+
+		$scope.canRotate = false;
+
+		$scope.toggleCanRotate = function() {
+			$scope.canRotate = !$scope.canRotate;
+		};
+
+		$scope.handlePadding = 5;
+		$scope.rotationalHandleRadius = 6;
+		$scope.rotationalHandleLength = 15;
+
+		$scope.selectedSelection = undefined;
+
+		$scope.draggingCanvas = false;
+
+		$scope.deselectSelection = function() {
+			$scope.selectedSelection = undefined;
+		};
+
+		$scope.selectSelection = function(selection) {
+			$scope.deselectSelection();
+			$scope.selectedSelection = selection;
+		};
+
+		$scope.isSelected = function(selection) {
+			return $scope.selectedSelection === selection;
+		};
+
+		$scope.removeSelection = function(selection) {
+			if(typeof(selection) === 'number') {
+				// The selection argument is the index.
+				$scope.selections.splice(selection, 1);
+			} else {
+				$scope.selections = $scope.selections.filter(function(someSelection) {
+					return someSelection !== selection;
+				});
+			}
+		};
+
+		$scope.removeSelectedSelection = function() {
+			$scope.selections.forEach(function(selection, index) {
+				if($scope.selectedSelection === selection) {
+					$scope.selections.splice(index, 1);
+				}
+			});
+			$scope.deselectSelection();
+		};
+
+		$scope.dragCreateSelection = function(offsetX, offsetY) {
+			$scope.dragging_canvas = false;
+			var selection = {
+				center_x: offsetX / $scope.image.width,
+				center_y: offsetY / $scope.image.height,
+				width: 0,
+				height: 0,
+				rotation: 0
+			};
+			$scope.selections.push(selection);
+			// Wait just a ms for the selection to bind.
+			setTimeout(function() {
+				$scope.$broadcast('onSelectionDragCreated', {
+					selection: selection,
+					offsetX: offsetX,
+					offsetY: offsetY
+				});
+			}, 1);
+		};
+
+		// Manipulating the canvas
+		$scope.mouseDown = function() {
+			$scope.draggingCanvas = true;
+			$scope.deselectSelection();
+		};
+
+		$scope.mouseUp = function() {
+			$scope.draggingCanvas = false;
+		};
+
+		$scope.mouseMove = function(offsetX, offsetY) {
+			var nothingSelected = typeof($scope.selectedSelection) === 'undefined';
+			if($scope.draggingCanvas && nothingSelected) {
+				$scope.dragCreateSelection(offsetX, offsetY);
+			}
+		};
+
+		/*
+
+		$scope.isValidSelection = function(selection) {
+			var valid_center_x = selection.center_x >= 0 && selection.center_x <= 1;
+			var valid_center_y = selection.center_y >= 0 && selection.center_y <= 1;
+			var valid_width = selection.width >= 0 && selection.width <= 1;
+			var valid_height = selection.height >= 0 && selection.height <= 1;
+			var valid_rotation = selection.rotation >= 0 && selection.rotation <= 2*Math.PI;
+			return valid_center_x && valid_center_y && valid_width && valid_height && valid_rotation;
+		};
+
+		// Validate selections as the selections array changes
+		// (not the selections them selves).
+		$scope.$watch('selections', function(selections) {
+			// Filter out any invalid selections.
+			selections = selections.filter($scope.isValidSelection);
+			// Update the selections scope variable again.
+			$scope.selections = selections;
+		}, true);
+		*/
+
+		/*
+
+
+
+
+		*/
+		$(window).keypress(function(e) {
+			if(e.keyCode === 127) { // The 'Delete' button
+				$scope.removeSelectedSelection();
+			}
+		});
+
+		// When clicking anywhere outside the canvas, deselect all selections.
+		$(document.body).click(function() {
+			$scope.$apply(function() {
+				$scope.deselectSelection();
+			});
+		});
+
+	}])
+	.directive('imageSelectionCanvas', function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'templates/image-selection/canvas.html',
+			templateNamespace: 'svg',
+			controller: 'imageSelectionCanvasCtrl',
+			scope: {
+				'imageSrc': '@src',
+				'selections': '='
+			},
+			link: function(scope, element, attrs) {
+				$image = element.find('img');
+				function updateThumbnailDimensions() {
+					scope.$apply(function() {
+						scope.image.width = $image.width();
+						scope.image.height = $image.height();
+						scope.image.loaded = true;
+					});
+				}
+				$image.bind('load', updateThumbnailDimensions);
+				$(window).bind('resize', updateThumbnailDimensions);
+				$(element).bind('click', function(e) {
+					// This prevents the click from deselecting.
+					e.stopPropagation();
+				});
+			}
+		};
+	})
+	.controller('imageSelectionCtrl', ['$scope', function($scope) {
+		// The corner in which 
+		$scope.grabbed = false;
+		$scope.grabbedAt = {
+			x: undefined,
+			y: undefined
+		};
+		$scope.grabbedHandleDirection = undefined;
+		$scope.grabbedOffset = {
+			top: undefined,
+			left: undefined,
+			bottom: undefined,
+			right: undefined
+		};
+
+		$scope.visible = false;
+		$scope.hovering = false;
+
+		$scope.deriveScope = function() {
+			var selection = $scope.selection;
+			var image = $scope.image;
+
+			$scope.maskId = 'Mask-' + $scope.$index;
+			$scope.left = (selection.center_x-selection.width/2) * image.width;
+			$scope.top = (selection.center_y-selection.height/2) * image.height;
+			$scope.width = selection.width * image.width;
+			$scope.height = selection.height * image.height;
+			$scope.centerX = selection.center_x * image.width;
+			$scope.centerY = selection.center_y * image.height;
+			$scope.rotationStyle = [
+				'transform: rotate(' +selection.rotation+ 'deg)',
+				'transform-origin: '+$scope.centerX+ 'px ' +$scope.centerY+ 'px'
+			].join(';');
+		};
+
+		$scope.handleGrabbed = function(direction, $event) {
+			var x = $event.offsetX / $scope.image.width;
+			var y = $event.offsetY / $scope.image.height;
+			// Make this selection, the selected selection.
+			$scope.$parent.selectSelection($scope.selection);
+			
+			$scope.grabbedAt = {
+				x: x,
+				y: y
+			};
+			// Save a copy of the selection when grapped.
+			$scope.grabbedOffset = {
+				center_x: $scope.selection.center_x,
+				center_y: $scope.selection.center_y,
+				width: $scope.selection.width,
+				height: $scope.selection.height,
+				rotation: $scope.selection.rotation
+			};
+			$scope.grabbed = true;
+			$scope.grabbedHandleDirection = direction;
+
+			// If this is infact a real $event and not just an object with
+			// offsetX and offsetY. Let's make sure this click does not propagate.
+			if($event.stopPropagation) {
+				$event.stopPropagation();
+			}
+		};
+
+		$scope.handleReleased = function() {
+			$scope.grabbed = false;
+			$scope.grabbedHandleDirection = undefined;
+		};
+
+		$scope.mouseMoved = function(x, y) {
+			// Normalize the coordinates.
+			x /= $scope.image.width;
+			y /= $scope.image.height;
+
+			if($scope.selected && $scope.grabbed) {
+				if($scope.grabbedHandleDirection === 'move') {
+					// Moving the whole selection.
+					$scope.selection.center_x = $scope.grabbedOffset.center_x - $scope.grabbedAt.x + x;
+					$scope.selection.center_y = $scope.grabbedOffset.center_y - $scope.grabbedAt.y + y;
+					// Keeing the bounds.
+					$scope.selection.center_x = Math.max($scope.selection.width/2, $scope.selection.center_x);
+					$scope.selection.center_y = Math.max($scope.selection.height/2, $scope.selection.center_y);
+					$scope.selection.center_x = Math.min(1-$scope.selection.width/2, $scope.selection.center_x);
+					$scope.selection.center_y = Math.min(1-$scope.selection.height/2, $scope.selection.center_y);
+				} else if($scope.grabbedHandleDirection === 'rotational') {
+					var diffX = $scope.selection.center_x - x;
+					var diffY = $scope.selection.center_y - y;
+					var radians = Math.atan2(diffY, diffX);
+					$scope.selection.rotation = (radians / Math.PI / 2 * 360) - 90;
+				} else {
+					var offsets = {
+						left: $scope.selection.center_x - $scope.selection.width/2,
+						top: $scope.selection.center_y - $scope.selection.height/2,
+						right: $scope.selection.center_x + $scope.selection.width/2,
+						bottom: $scope.selection.center_y + $scope.selection.height/2,
+					};
+
+					// TODO: Switch around north and south and east and west.
+					/*
+					if(offsets.left < x) {
+						$scope.grabbedHandleDirection = $scope.grabbedHandleDirection.replace('west', 'east');
+					} else if(offsets.right > x) {
+						$scope.grabbedHandleDirection = $scope.grabbedHandleDirection.replace('east', 'west');
+					}
+					if(offsets.top < y) {
+						$scope.grabbedHandleDirection = $scope.grabbedHandleDirection.replace('south', 'north');
+					} else if(offsets.bottom > y) {
+						$scope.grabbedHandleDirection = $scope.grabbedHandleDirection.replace('north', 'south');
+					}
+					*/
+
+					if($scope.grabbedHandleDirection.indexOf('north') >= 0) {
+						offsets.top = y;
+					}
+					if($scope.grabbedHandleDirection.indexOf('east') >= 0) {
+						offsets.right = x;
+					}
+					if($scope.grabbedHandleDirection.indexOf('south') >= 0) {
+						offsets.bottom = y;
+					}
+					if($scope.grabbedHandleDirection.indexOf('west') >= 0) {
+						offsets.left = x;
+					}
+					$scope.selection.center_x = offsets.left+(offsets.right-offsets.left)/2;
+					$scope.selection.center_y = offsets.top+(offsets.bottom-offsets.top)/2;
+					$scope.selection.height = Math.max(offsets.bottom - offsets.top, 0 );
+					$scope.selection.width = Math.max(offsets.right - offsets.left, 0 );
+				}
+			}
+		};
+
+		// Derive the scope when the image loads.
+		$scope.$watch('image.loaded', function(nowLoaded, oldLoaded) {
+			var justLoaded = nowLoaded && nowLoaded !== oldLoaded;
+			if(justLoaded) {
+				$scope.deriveScope();
+			}
+		});
+
+		// Or when the selection changes.
+		$scope.$watch('selection', $scope.deriveScope, true);
+
+		// When the selected selection changes
+		$scope.$parent.$watch('selectedSelection', function(selectedSelection) {
+			// Propagate this value to the current scope
+			$scope.selected = selectedSelection === $scope.selection;
+			// And hide this outline if something was selected and it wasn't this.
+			$scope.visible = typeof(selectedSelection) === 'undefined' || $scope.selected;
+		});
+
+		$scope.$on('onSelectionDragCreated', function(e, args) {
+			if(args.selection === $scope.selection) {
+				// This is the selection that was just added.
+				$scope.handleGrabbed('south-east', {
+					offsetX: args.offsetX,
+					offsetY: args.offsetY
+				});
+			}
+		});
+	}])
+	.directive('imageSelectionOutline', function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'templates/image-selection/outline.html',
+			replace: true,
+			templateNamespace: 'svg',
+			controller: 'imageSelectionCtrl'
+		};
+	});
+})();
