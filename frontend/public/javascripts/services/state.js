dedf3251073cd4f8cd3fb394b94d4f475ee9cbc4
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2015-05-14 16:08:01 +0200

    Adding a state service and a view for state jobs.

---
+++ frontend/public/javascripts/services/state.js
@@ -0,0 +1,29 @@
+angular
+	.module('cropper')
+	.factory('state', ['$q', '$http', function($q, $http) {
+		var BASE_URL = '/state';
+		return {
+			get: function() {
+				return $http.get(BASE_URL+'/')
+				.then(function(response) {
+					return response.data;
+				});
+			},
+			longPoll: function(currentStateRevision) {
+				/*
+				return $http({
+					url: BASE_URL+'/long-poll/',
+					timeout: 5000
+				})
+				*/
+				if(!currentStateRevision) {
+					currentStateRevision = '';
+				}
+
+				return $http.get(BASE_URL+'/long-poll/' + currentStateRevision)
+				.then(function(response) {
+					return response.data;
+				});
+			}
+		};
+	}]);
