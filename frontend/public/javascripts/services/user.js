62b4fd5bb38f6022913b3de189e94c0a0841eec1
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2015-06-23 22:48:03 +0200

    Cleaning up some comments and a console.error message.

--- frontend/public/javascripts/services/user.js
+++ frontend/public/javascripts/services/user.js
@@ -3,6 +3,7 @@ angular
 	.factory('user', [
 		'$q', 'store', 'jwtHelper', 'auth',
 		function($q, store, jwtHelper, auth) {
+
 			function login() {
 				var deferred = $q.defer();
 
@@ -16,7 +17,7 @@ angular
 						token: token
 					});
 				}, function(err) {
-					console.error("Error :(", err);
+					console.error("Error logging in:", err);
 					deferred.reject('Could not sign in: ' + err);
 				});
 
@@ -24,29 +25,36 @@ angular
 			}
 
 			return {
+				// Get the authenticated user.
 				get: function() {
 					var deferred = $q.defer();
 
 					var token = store.get('token');
+					// Do we already have a token, i.e. is the user logged in?
 					if (token) {
+						// Is this token still fresh?
 						if (!jwtHelper.isTokenExpired(token)) {
+							// If auth don't know yet - let's tell
 							if (!auth.isAuthenticated) {
 								auth.authenticate(store.get('profile'), token);
 							}
-							// Resolve the promise with this.
+							// Resolve the promise with this users profile and token.
 							deferred.resolve({
 								profile: store.get('profile'),
 								token: token
 							});
 						} else {
+							// The users action is needed.
 							deferred.resolve( login() );
 						}
 					} else {
+						// The users action is needed.
 						deferred.resolve( login() );
 					}
 
 					return deferred.promise;
 				},
+				// Sign out and forget about the user.
 				forget: function() {
 					auth.signout();
 					store.remove('profile');
